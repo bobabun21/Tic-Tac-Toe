eec0f6fc7a6247956ed798002772e8e9ac3d96b8
Kelsey <kbelanger@catalyte.io>
2024-07-11 16:43:49 -0400

    fix single quotes issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 export default function Square() {
   return (
-    <button type="button" className="square">
+    <button type='button' className='square'>
       X
     </button>
   );
